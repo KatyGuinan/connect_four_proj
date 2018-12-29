955414bef2edc96ada38959d06c343a37cf1db0e
Katy Guinan <katy@interactiveos.com>
2018-12-28 22:27:51 -0500

    fox

--- greeting.js
+++ greeting.js
@@ -1,4 +1,8 @@
 var pOne = prompt("Player One: Enter Your Name, you will be Blue")
 var pTwo = prompt("Player Two: Enter Your Name, you will be Red")
 
+if (pOne === pTwo) {
+  pTwo = pTwo+'2'
+}
+
 var turn = pOne
